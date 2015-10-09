49d67235738032bdaa9f493e1c0d28fc8799b2d1
Shannon Moeller <me@shannonmoeller.com>
2015-10-09 00:31:40 -0400

    corrected paths

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,8 +5,18 @@ var yeoman = require('yeoman-generator');
 module.exports = yeoman.generators.Base.extend({
 	writing: function () {
 		this.fs.copy(
-			this.templatePath('{.,src,tasks}/**/{.*,*.*}'),
+			this.templatePath('{.*,*.*}'),
 			this.destinationRoot()
+		);
+
+		this.fs.copy(
+			this.templatePath('src'),
+			this.destinationPath('src')
+		);
+
+		this.fs.copy(
+			this.templatePath('tasks'),
+			this.destinationPath('tasks')
 		);
 	},
 
