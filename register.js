a5a9909dae383eaeac7b0e163848c8bc754bb69e
Shannon Moeller <me@shannonmoeller.com>
2016-06-04 17:04:04 -0400

    Replaced esprev with babel because the register hook was not working

--- register.js
+++ register.js
@@ -1,3 +1,16 @@
 'use strict';
 
-require('esprev/register');
+var presets = [
+    require.resolve('babel-preset-es2015'),
+    require.resolve('babel-preset-stage-2'),
+];
+
+var plugins = [
+    require.resolve('babel-plugin-add-module-exports'),
+];
+
+require('babel-polyfill');
+require('babel-register')({
+    presets: presets,
+    plugins: plugins,
+});
